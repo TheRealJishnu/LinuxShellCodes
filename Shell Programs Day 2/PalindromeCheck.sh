7e394e32be6561b734f4b2895ae068097f5af72a
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2022-08-25 23:17:23 +0530

    Update PalindromeCheck.sh

--- Shell Programs Day 2/PalindromeCheck.sh
+++ Shell Programs Day 2/PalindromeCheck.sh
@@ -2,17 +2,22 @@
 
 echo "Enter A Number	:	"
 read num
+show=`expr $num`
 reverse=0
-temp=0
-while [ $num -ne 0 ]
+# rem=0
+while [ $num -gt 0 ]
 do 
-	reverse=`expr temp \* 10`
-	temp=`expr num \* 10`
-	reverse=`expr temp
+	reverse=`expr $reverse \* 10`
+	rem=`expr $num % 10`
+	reverse=`expr $reverse + $rem`
+
 	num=`expr $num / 10`
 done
 
-echo $reverse
-
-
-
+# echo "Reverse is " $reverse
+if [ $show -eq $reverse ]
+then
+	echo $show " is a palindrome number"
+else
+	echo $show " is not a palindrome number"
+fi
