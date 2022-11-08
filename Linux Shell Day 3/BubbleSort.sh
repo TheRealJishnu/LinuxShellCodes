cbb28fc5c0b59f8038d3a43f16f0b333cdbae61b
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2022-11-08 15:56:36 +0530

    Final Working Script

---
+++ Linux Shell Day 3/BubbleSort.sh
@@ -0,0 +1,29 @@
+echo "Enter Size of Array: "
+read n
+echo "Enter Array Elements : "
+for(( i=0; i<n; i++))
+do
+	read arr[$i]
+done
+
+echo "Unsorted Array : "
+echo ${arr[*]}
+
+for(( i=0; i<n-1 ; i++))
+do
+	for(( j=0; j<n-i-1; j++ ))
+	do
+		if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
+		then
+			# swap
+			temp=${arr[j]}
+			arr[$j]=${arr[$((j+1))]}  
+			arr[$((j+1))]=$temp
+		fi
+	done
+done
+
+echo "Sorted Array: "
+echo ${arr[*]}
+	
+	 
